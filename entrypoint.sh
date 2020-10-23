bb279d8e6387e8f5a5321bb5348188350375da75
lumoTime <66256045+lumotime@users.noreply.github.com>
2020-10-23 11:41:07 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+gitLog = $(git log)
+echo "$gitLog"
 
 if [[ $1 != '' ]]
 then
+echo("then")
 echo "tag is $1"
 log=$(git log $1.. --pretty=format:'- %cd %an \n  %s\n \n' --date=format:'%Y-%m-%d %H:%M:%S')
 
 else
+echo("else")
 tagName=$(git describe --abbrev=0 --tags)
 echo "tag is $tagName"
 log=$(git log $tagName.. --pretty=format:'- %cd %an \n  %s\n \n' --date=format:'%Y-%m-%d %H:%M:%S')
